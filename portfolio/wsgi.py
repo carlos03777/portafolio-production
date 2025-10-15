5c6486cc589b2142a35f8a1611791c8693732f1a
carlos <carlosforero2025777@gamil.com>
2025-10-15 01:10:20 -0500

    uop

--- portfolio/wsgi.py
+++ portfolio/wsgi.py
@@ -1,16 +1,10 @@
-"""
-WSGI config for portfolio project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
-"""
-
+from django.core.wsgi import get_wsgi_application
+from whitenoise import WhiteNoise
+from pathlib import Path
 import os
 
-from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'portfolio.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "portfolio.settings")
 
 application = get_wsgi_application()
+BASE_DIR = Path(__file__).resolve().parent.parent
+application = WhiteNoise(application, root=BASE_DIR / "staticfiles")
